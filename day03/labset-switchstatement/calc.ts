a687672a3ceda7b230e8157d44fa50665db2099f
PD <168636679+chsny1pd@users.noreply.github.com>
2025-12-08 18:17:00 +0700

    calc.ts

---
+++ day03/labset-switchstatement/calc.ts
@@ -0,0 +1,32 @@
+// const op = process.argv[2]
+// const a = +process.argv[3]
+// const b = +process.argv[4]
+
+// switch(op)
+// {
+//   case 'add' :
+//     const addresult = a+b
+//     console.log(addresult)
+//     break;
+//   case 'sub' :
+//     const subresult = a-b
+//     console.log(subresult)
+//     break;
+//   case 'mul' :
+//     const mulresult = a*b
+//     console.log(mulresult)
+//     break;
+//   case 'div' :
+//     if(b==0)
+//     {
+//       console.log('Zero is a divisor, cannot be found.')
+//       break;
+//     }
+//     else
+//     {
+//      const divresult = a/b
+//      console.log(divresult)
+//      break;
+//     }
+//   default : console.log('Invalid Operation')
+// }
