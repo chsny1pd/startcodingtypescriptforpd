f6cf4c3447a154c0ef898024302c8ff72626eb64
PD <168636679+chsny1pd@users.noreply.github.com>
2025-12-09 20:56:58 +0700

    pattern 8 + 9

---
+++ day03/practice-number-patterns/pattern8.ts
@@ -0,0 +1,22 @@
+// const input = +process.argv[2]
+
+// if(Number.isNaN(input) || input<=0)
+// {
+//   process.exit()
+// }
+
+// for(let i = 0;i<input;i++)
+// {
+//   let result = ''
+//   let k = input-i
+//   for(let j = 0;j<input;j++)
+//   {
+//     if(j==i || j==k-1){
+//     result += '1'  
+//     }
+//     else{
+//     result += '0'
+//     }
+//   }
+//   console.log(result)
+// }
---
+++ day03/practice-number-patterns/pattern9.ts
@@ -0,0 +1,32 @@
+// const input = +process.argv[2]
+
+// if(Number.isNaN(input) || input<=0)
+// {
+//   process.exit()
+// }
+// for(let i = 1;i<=input;i++)
+// {
+//   let result = ''
+//   if(i==1 || i==input){
+//   for(let j = 0;j<input;j++)
+//   {
+//     if(j==0 || j==input-1){
+//     result += '0'  
+//     }
+//     else{
+//     result += '1'
+//     }
+//   }
+//   }
+//   else{
+//   for(let j = 1;j<=input;j++)
+//   {
+//     if(j==1 || j==input){
+//     result += '1'  
+//     }
+//     else{
+//     result += '0'
+//     }
+//   }}
+//   console.log(result)
+// }
