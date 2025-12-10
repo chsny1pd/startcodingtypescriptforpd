44f184b71051b505b3fd6fb3a614d77aed6c3422
PD <168636679+chsny1pd@users.noreply.github.com>
2025-12-10 09:22:18 +0700

    //

--- day03/labset-loopandconditioncombined/lc5.ts
+++ day03/labset-loopandconditioncombined/lc5.ts
@@ -1,27 +1,27 @@
-const inputs = process.argv.slice(2)
+// const inputs = process.argv.slice(2)
 
-let index = 0;
-let value: any;
+// let index = 0;
+// let value: any;
 
-do {
-  value = inputs[index];
+// do {
+//   value = inputs[index];
 
-  if (value?.includes('admin')) 
-  {
-    console.log(`${value} ->  Full Access`);
-  }
-  else if (value?.includes('user')) 
-  {
-    console.log(`${value} ->  Limited Access`);
-  }
-  else if (value?.includes('guest')) 
-  {
-    console.log(`${value} ->  Public Access`);
-  }
-  else if (value?.includes('unknown')) 
-  {
-    console.log(`${value} ->  No Access`);
-  }
-  else{}
-  index++;
-} while (value != undefined);
+//   if (value?.includes('admin')) 
+//   {
+//     console.log(`${value} ->  Full Access`);
+//   }
+//   else if (value?.includes('user')) 
+//   {
+//     console.log(`${value} ->  Limited Access`);
+//   }
+//   else if (value?.includes('guest')) 
+//   {
+//     console.log(`${value} ->  Public Access`);
+//   }
+//   else if (value?.includes('unknown')) 
+//   {
+//     console.log(`${value} ->  No Access`);
+//   }
+//   else{}
+//   index++;
+// } while (value != undefined);
